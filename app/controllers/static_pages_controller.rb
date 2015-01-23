fbc7b76c65808e140c958d920e99ee58987de1d5
anhnt560 <tuananh250993@gmail.com>
2015-01-23 13:36:45 +0700

    Finish microposts

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,17 @@
 class StaticPagesController < ApplicationController
+
   def home
+    if logged_in?
+      @micropost  = current_user.microposts.build
+      #@current_feed_items = current_user.feed.paginate(page: params[:page])
+    end
+    posts = Micropost.all
+    @feed_items = posts.paginate(page: params[:page])
+  end
+
+  def help
+  end
+
+  def about
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,8 +3,13 @@ class UsersController < ApplicationController
     @user = User.new
   end
   
+  def index
+    @users=User.paginate(page: params[:page])
+  end
+  
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
   
   def create
